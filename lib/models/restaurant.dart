bf8a2a8e609705b609d976d2cf2365f8f0869795
Maksim11011 <mvbspb@bk.ru>
2024-03-22 18:28:43 +0300

    add getter cart

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -317,6 +317,7 @@ class Restaurant extends ChangeNotifier {
 */
 
   List<Food> get menu => _menu;
+  List<CartItem> get cart => _cart;
 
 /*
 
---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:provider/provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Restaurant>(
+      builder: (context, restaraunt, child) {},
+    );
+  }
+}
